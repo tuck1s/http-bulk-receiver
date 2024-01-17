6d6a8f41a21e973c9258dbf2e5ac3c8b04b57f0d
tuck1s <steve@thetucks.com>
2024-01-17 18:09:12 +0000

    Detail log of json payload

--- http-bulk-receiver.go
+++ http-bulk-receiver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -16,9 +17,9 @@ func main() {
 	sleepDuration := flag.Int("sleep", 0, "Sleep duration in milliseconds before responding with 200 OK")
 	flag.Parse()
 
-	http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Received %s request from %s\n", r.Method, r.RemoteAddr)
+	log.SetOutput(os.Stdout)
 
+	http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			fmt.Printf("Invalid request method: %s\n", r.Method)
 			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -30,36 +31,49 @@ func main() {
 			http.Error(w, "Error reading request body", http.StatusInternalServerError)
 			return
 		}
-
 		// Log the request to a file with a timestamp in the filename
-		err = logRequest(body)
+		err = storeRequestBody(body)
 		if err != nil {
 			http.Error(w, "Error logging request", http.StatusInternalServerError)
 			return
 		}
 
+		var requestBody interface{}
+		jsonInfo := ""
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			jsonInfo = err.Error()
+		} else {
+			// Check if the unmarshaled value is an array
+			if arr, ok := requestBody.([]interface{}); ok {
+				// It's an array, print its length
+				jsonInfo = fmt.Sprintf("array length: %d", len(arr))
+			} else {
+				// It's not an array
+				fmt.Println("not an array.")
+			}
+		}
+		log.Printf("Received %s request from %s with body size %d bytes, JSON %s", r.Method, r.RemoteAddr, len(body), jsonInfo)
+
 		// Sleep for the specified duration before responding with 200 OK
 		time.Sleep(time.Duration(*sleepDuration) * time.Millisecond)
 
-		fmt.Fprint(w, "Request logged successfully")
+		fmt.Fprint(w, "OK")
 	})
 
 	fmt.Printf("Server is listening on port %s, path: %s, sleep duration: %d milliseconds...\n", *port, *path, *sleepDuration)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
-func logRequest(body []byte) error {
+func storeRequestBody(body []byte) error {
 	timestamp := time.Now().Format("2006-01-02_15-04-05")
-	filename := fmt.Sprintf("request_%s.log", timestamp)
+	filename := fmt.Sprintf("request_%s.json", timestamp) // incoming requests to have JSON payload
 
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
-	log.SetOutput(file)
-	log.Println(string(body))
-
-	return nil
+	_, err = file.Write(body)
+	return err
 }
